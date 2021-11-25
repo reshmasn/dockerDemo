e558ca74a31cbcc80192c030da136899e3b10013
reshmasn <reshmaashika10@gmail.com>
2021-11-25 23:52:45 +0530

    Committing the code done

---
+++ src/main/java/com/LTI/dockerkubernetesdemo/DockerKubernetesDemoApplication.java
@@ -0,0 +1,14 @@
+package com.LTI.dockerkubernetesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerKubernetesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerKubernetesDemoApplication.class, args);
+		
+	}
+
+}
---
+++ src/main/java/com/LTI/dockerkubernetesdemo/controller/dockerController.java
@@ -0,0 +1,12 @@
+package com.LTI.dockerkubernetesdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class dockerController {
+	@GetMapping("/demo")
+	public String getData() {
+	return "The application is running successfully";
+	}
+}
